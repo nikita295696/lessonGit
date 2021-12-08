bee1e4d2f29e404c079fcb23a847148e213236b3
student <43537358+nickdpua@users.noreply.github.com>
2021-12-08 18:37:17 +0200

    Added folder navigation

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-!<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,14 +9,37 @@
 </head>
 <body>
 
+<?php
+session_start();
+$currentFolder = $_SESSION['folder'] ?? realpath('uploads');
+
+if(isset($_GET['folder'])) {
+    $_SESSION['folder'] = $_SESSION['folder'] . "/" . $_GET['folder'];
+    $_SESSION['folder'] = realpath($_SESSION['folder']);
+    header("Location: index.php");
+}
+
+chdir($_SESSION['folder']);
+
+?>
+
 <form id="create_folder" method="post">
     <input type="text" name="folder_name" placeholder="Folder Name">
     <button type="submit">Create folder</button>
 </form>
 
 <ul>
-    <li><a href="#">Folder 1</a> </li>
-    <li><a href="#">File</a> </li>
+    <?php
+        foreach(scandir($_SESSION['folder']) as $item) {
+            if($item !== '.' && $item !== '.gitignore' && !(basename($_SESSION['folder']) === 'uploads' && $item === '..')) {
+                if(is_dir($item)) {
+                    ?> <li><a href="?folder=<?= $item ?>"><?= $item ?></a></li> <?php
+                } else {
+                    ?> <li><a href="#" download="<?= $item ?>"><?= $item ?></a></li> <?php
+                }
+            }
+        } 
+    ?>
 </ul>
 
 <form method="post" enctype="multipart/form-data">
